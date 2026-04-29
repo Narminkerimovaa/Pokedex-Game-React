2d4d650cdefe36c9a46112a58ed698d621d1521a
Narminkerimova <narmingk-bf206@code.edu.az>
2026-04-29 21:20:35 +0400

    progress: design mostly completed

--- src/components/battle/BattleArena.jsx
+++ src/components/battle/BattleArena.jsx
@@ -1,13 +1,21 @@
-import Card from "../ui/Card";
 import styles from "./Battle.module.css";
+import PokeTeam from "./PokeTeam";
 
 const BattleArena = ({ teamName, totalXP, status }) => {
   return (
-    <Card variant="teamBox" className={styles.battleArena}>
-      <div className={styles.teamHeader}>{teamName}</div>
-      <div className={styles.scoreText}>{totalXP} XP</div>
-      <div className={styles.statusText}>{status}</div>
-    </Card>
+    <div className={styles.battleArena}>
+      <div className={styles.headText}>
+        <h3 className={styles.teamHeader}>{teamName}</h3>
+        <div className={styles.scoreText}>{totalXP} XP</div>
+        <div className={styles.winLabel}>{status}</div>
+      </div>
+      <div className={styles.cardGrid}>
+        <PokeTeam />
+        <PokeTeam />
+        <PokeTeam />
+        <PokeTeam />
+      </div>
+    </div>
   );
 };
 
