2d992a4fa5727473e171f32b4f8abbd085086e51
Narminkerimova <narmingk-bf206@code.edu.az>
2026-04-29 21:21:36 +0400

    create api.js and utils.js

---
+++ src/utils/battleUtil.js
@@ -0,0 +1,36 @@
+import { pokemon } from "./../constants/pokemon";
+
+export function distributePokemon() {
+  let temp = [...pokemon];
+  let team1 = [];
+  let team2 = [];
+
+  let toggle = true;
+
+  while (temp.length > 0) {
+    let index = Math.floor(Math.random() * temp.length);
+
+    let picked = temp[index];
+
+    if (toggle) {
+      team1.push(picked);
+    } else {
+      team2.push(picked);
+    }
+
+    temp.splice(index, 1);
+    toggle = !toggle;
+  }
+
+  return { team1, team2 };
+}
+
+export function calcXP(team) {
+  return team.reduce((acc, curr) => acc + curr.base_experience, 0);
+}
+
+export function getWinner(t1, t2) {
+  return t1 > t2 ? "team1" : "team2";
+}
+
+
